c54273400f03c27c071179c7d6ac13d7d945c5ba
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-07-30 13:28:24 -0400

    configure paperclip s3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => '300x300' }
+  has_attached_file :image, :styles => { :medium => '300x300' },
+                            :storage => :s3,
+                            :s3_credentials => S3_CREDENTIALS,
+                            :bucket => S3_BUCKET,
+                            :path => "images/:id.:extension"
 
   CATEGORIES = ['Necklace', 'Bracelet', 'Earring', 'Ring', 'Hair Accessories']
 end
